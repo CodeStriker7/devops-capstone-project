6da58a1bc78ede1148503740a70a1c583d2bc398
Ozod <djozod51@gmail.com>
2026-06-04 23:40:48 +0500

    fix: format code to pass flake8 linting

--- service/routes.py
+++ service/routes.py
@@ -62,14 +62,13 @@ def create_accounts():
 ######################################################################
 
 
-
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     app.logger.info("Request to list all accounts")
-    
+
     accounts = Account.all()
     results = [account.serialize() for account in accounts]
-    
+
     return make_response(jsonify(results), status.HTTP_200_OK)
 
 
@@ -78,15 +77,14 @@ def list_accounts():
 ######################################################################
 
 
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     app.logger.info("Request to read an Account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-        
+
     return make_response(jsonify(account.serialize()), status.HTTP_200_OK)
 
 ######################################################################
@@ -94,19 +92,18 @@ def get_accounts(account_id):
 ######################################################################
 
 
-
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """Mavjud akkauntni yangilash"""
     app.logger.info("Request to update an Account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-        
+
     account.deserialize(request.get_json())
     account.update()
-    
+
     return make_response(jsonify(account.serialize()), status.HTTP_200_OK)
 
 ######################################################################
@@ -114,16 +111,15 @@ def update_accounts(account_id):
 ######################################################################
 
 
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """Akkauntni o'chirish"""
     app.logger.info("Request to delete an Account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
     if account:
         account.delete()
-        
+
     return make_response("", status.HTTP_204_NO_CONTENT)
 
 ######################################################################
